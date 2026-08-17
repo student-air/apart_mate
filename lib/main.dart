a2f877b9969fa1d6a56878ca699a81144e25314d
student-air <230972@students.au.edu.pk>
2026-08-17 23:21:54 +0500

    Firbase removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:apart_mate/firebase_options.dart';
 import 'package:apart_mate/core/theme/app_theme.dart';
 import 'package:apart_mate/core/bindings/initial_binding.dart';
 import 'package:apart_mate/core/constants/app_strings.dart';
@@ -9,14 +7,12 @@ import 'package:apart_mate/routes/app_pages.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const apart_mateApp());
+
+  runApp(const ApartMateApp());
 }
 
-class apart_mateApp extends StatelessWidget {
-  const apart_mateApp({super.key});
+class ApartMateApp extends StatelessWidget {
+  const ApartMateApp({super.key});
 
   @override
   Widget build(BuildContext context) {
